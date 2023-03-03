62b3064ea12880a7c839da19a48cb5e5f647c166
mshabunin97 <120470036+mshabunin97@users.noreply.github.com>
2023-03-03 16:37:13 +0300

    Add files via upload

---
+++ jhon/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu__body'),
+    menuItem = document.querySelectorAll('.menu__list'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger__active');
+        menu.classList.toggle('menu__active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger__active');
+            menu.classList.toggle('menu__active');
+        })
+    })
+});
